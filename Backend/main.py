9a5b08d45f3570f2b0aa76d38b8caa5ca49c9aba
parthgenx <parthgenx@gmail.com>
2025-12-18 16:20:24 +0530

    Fixed model

--- Backend/main.py
+++ Backend/main.py
@@ -158,7 +158,7 @@ def generate_chat(request: ChatRequest):
         """
 
         response = client.chat.completions.create(
-            model="meta-llama/llama-4-scout-17b-16e-instruct",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": request.prompt}
